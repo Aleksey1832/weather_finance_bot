a0863fe807b7fc4de6bbe112feadc9d9294d2d6f
Aleksey1832 <alekseykhitrov@gmail.com>
2026-03-12 20:18:11 +0300

    12.03.26 - добавлен класс OilFormatter

--- services_finance/finance_data/oil_data.py
+++ services_finance/finance_data/oil_data.py
@@ -2,51 +2,55 @@ from datetime import datetime
 import pandas as pd
 
 
-def get_oil_price(oil_table):
+class OilFormatter:
     """
     Получает отчет по нефти, формирует отчет и возвращает отформатированную строку.
     """
-    # 1. Защита: Если данных нет совсем.
-    if oil_table is None or oil_table.empty:
-        return "🛢 <b>Нефть Brent:</b> <i>Биржа закрыта или нет связи</i>"
 
-    try:
-        # 2. Защита: Проверка наличие колонок (чтобы не вылететь по KeyError).
-        if 'Close' not in oil_table.columns or 'Open' not in oil_table.columns:
-            return "🛢 <b>Нефть:</b> <i>Ошибка структуры данных</i>"
+    @staticmethod
+    def get_oil_report(oil_table):
 
-        # 3. Достает последние строку и её дату (значения из таблицы).
-        last_row = oil_table.iloc[-1]
+        # 1. Защита: Если данных нет совсем.
+        if oil_table is None or oil_table.empty:
+            return "🛢 <b>Нефть Brent:</b> <i>Биржа закрыта или нет связи</i>"
 
-        # 4. Если в последней строке вместо цифры - пусто NaN (Not a Number).
-        if pd.isna(last_row['Close']) or pd.isna(last_row['Open']):
-            return "🛢 <b>Нефть:</b> <i>Данные обновляются...</i>"
+        try:
+            # 2. Защита: Проверка наличие колонок (чтобы не вылететь по KeyError).
+            if 'Close' not in oil_table.columns or 'Open' not in oil_table.columns:
+                return "🛢 <b>Нефть:</b> <i>Ошибка структуры данных</i>"
 
-        # 5. oil_table.index[-1] — это время закрытия торгов в формате Timestamp.
-        last_dt = oil_table.index[-1]
+            # 3. Достает последние строку и её дату (значения из таблицы).
+            last_row = oil_table.iloc[-1]
 
-        last_price = float(last_row['Close'])
-        open_price = float(last_row['Open'])
+            # 4. Если в последней строке вместо цифры - пусто NaN (Not a Number).
+            if pd.isna(last_row['Close']) or pd.isna(last_row['Open']):
+                return "🛢 <b>Нефть:</b> <i>Данные обновляются...</i>"
 
-        # 6. Сравнение даты для заголовка.
-        today = datetime.now().date()
-        is_today = last_dt.date() == today
+            # 5. oil_table.index[-1] — это время закрытия торгов в формате Timestamp.
+            last_dt = oil_table.index[-1]
 
-        # 7. Форматирует дату для вывода.
-        date_str = last_dt.strftime('%H:%M, %d.%m.%Y')
+            last_price = float(last_row['Close'])
+            open_price = float(last_row['Open'])
 
-        # 8. Если сегодня выходной, добавляет дату.
-        if is_today:
-            label = "Актуальный тикер нефти Brent:\n\n"
-        else:
-            label = f"Последняя цена Brent {date_str}:\n⚠️ <i>Торги приостановлены (выходной)</i>\n"
+            # 6. Сравнение даты для заголовка.
+            today = datetime.now().date()
+            is_today = last_dt.date() == today
 
-        status = "<b>Котировки фьючерса нефти</b>\n"
-        icon = "📈" if last_price >= open_price else "📉"
-        change_pct = ((last_price - open_price) / open_price) * 100
+            # 7. Форматирует дату для вывода.
+            date_str = last_dt.strftime('%H:%M, %d.%m.%Y')
 
-        return f"✅ <b>{status}{label}</b> 🛢${last_price:.2f} {icon} ({change_pct:+.2f}%)"
+            # 8. Если сегодня выходной, добавляет дату.
+            if is_today:
+                label = "Актуальный тикер нефти Brent:\n\n"
+            else:
+                label = f"Последняя цена Brent {date_str}:\n⚠️ <i>Торги приостановлены (выходной)</i>\n"
 
-    except Exception as e:
-        print(f"Ошибка парсинга нефти: {e}")
-        return "🛢 <b>Нефть:</b> <i>❌ Технический сбой</i>"
+            status = "<b>Котировки фьючерса нефти</b>\n"
+            icon = "📈" if last_price >= open_price else "📉"
+            change_pct = ((last_price - open_price) / open_price) * 100
+
+            return f"✅ <b>{status}{label}</b> 🛢${last_price:.2f} {icon} ({change_pct:+.2f}%)"
+
+        except Exception as e:
+            print(f"Ошибка парсинга нефти: {e}")
+            return "🛢 <b>Нефть:</b> <i>❌ Технический сбой</i>"
