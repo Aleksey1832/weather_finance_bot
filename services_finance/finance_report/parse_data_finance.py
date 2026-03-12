476b43596cfe74e7f06a991b8357a7fc49787fce
Aleksey1832 <alekseykhitrov@gmail.com>
2026-03-12 20:18:43 +0300

    12.03.26 - добавлен класс OilFormatter

--- services_finance/finance_report/parse_data_finance.py
+++ services_finance/finance_report/parse_data_finance.py
@@ -1,49 +1,45 @@
 from services_finance.finance_api.currency_api import CurrencyDataManager
 from services_finance.finance_api.crypto_api import CryptoDataManager
 from services_finance.finance_api.oil_api import OilDataManager
-from services_finance.finance_api.metal_api import MetalDataManager
 from services_finance.finance_data.metal_data import MetalFormatter
 from services_finance.finance_data.currency_data import get_currency_report
 from services_finance.finance_data.crypto_data import get_crypto_report
-from services_finance.finance_data.oil_data import get_oil_price
+from services_finance.finance_data.oil_data import OilFormatter
 
 
 def get_full_finance_report():
     """
     Формирование отчета.
     """
-    # 1. Создание финансов.
-    # 1.1 Создание валютчика и обновление.
+    # --- Создание финансов ---
+    # 1 Создание валютчика и обновление.
     currency_manager = CurrencyDataManager()
     currency_manager.update_rates()  # Это единственный запрос к ЦБ!
 
-    # 1.2 Создание крипты и обновление.
+    # 2 Создание крипты и обновление.
     crypto_manager = CryptoDataManager()
     crypto_manager.update_rates()  # Это единственный запрос к CoinGecko!
 
-    # 1.3 Создание запроса на нефть.
+    # 3. Курс usd для крипты.
+    usd_value = currency_manager.get_usd_value()
+
+    # 4. Создание запроса на нефть.
     manager = OilDataManager()
     oil_table = manager.fetch_oil_price()
 
-    # 1.4 Создание запроса на металлы.
-    metal_manager = MetalDataManager()
-    gold_t, silver_t = metal_manager.fetch_metals()
-
-    # 2. Курс usd для крипты.
-    usd_value = currency_manager.get_usd_value()
-
-    # 3. Раздача данных функциям.
-    # 3.1 Передача всего объекта в отчет по валютам.
+    # --- СБОРКА ТЕКСТОВЫХ БЛОКОВ ---
+    # Раздача данных функциям.
+    # 4. Валютный отчет. Передача всего объекта в отчет по валютам.
     currency = get_currency_report(currency_manager)
 
-    # 3.2 Передача всего объекта и числа (курс usd) в отчет по крипте.
+    # 5. Крипто отчет. Передача всего объекта и числа (курс usd) в отчет по крипте.
     crypto = get_crypto_report(crypto_manager, usd_value)
 
-    # 3.3 Передача всего объекта в отчет по нефти.
-    oil = get_oil_price(oil_table)
+    # 6. Нефтяной отчет. Функция сама создаст менеджера внутри и сходит на MOEX.
+    oil = OilFormatter.get_oil_report(oil_table)
 
-    # 3.4 Передача всего объекта в отчет по металлам.
-    metals = MetalFormatter.get_metals_report(gold_t, silver_t, usd_value)
+    # 7. Металлы. Форматтер сам разберется с ЦБ РФ внутри.
+    metals = MetalFormatter.get_metals_report()
 
     return (
         "<b>📊 ФИНАНСОВЫЙ ОТЧЕТ</b>\n"
