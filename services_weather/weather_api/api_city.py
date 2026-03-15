94042039256f9a6d8e02e2ad2fb93fae2b04e1d2
Aleksey1832 <alekseykhitrov@gmail.com>
2026-03-15 15:27:15 +0300

    15.03.26 Добавлена проверка статуса ответа

--- services_weather/weather_api/api_city.py
+++ services_weather/weather_api/api_city.py
@@ -9,6 +9,8 @@ def get_weather_by_city(city_name):
     """
     logger = logging.getLogger(__name__)
     try:
+        # 1. Формирование параметров словаря params с названием города,
+        # API-ключом и настройками формата (метрическая система, русский язык).
         params = {
             "q": city_name,
             "appid": open_weather_token,
@@ -16,11 +18,23 @@ def get_weather_by_city(city_name):
             "lang": "ru"
         }
 
+        # 2. Выполнение GET-запроса.
         response = requests.get(WEATHER_BASE_URL, params=params, timeout=10)
         logger.info(f"Ответ но названию города получен. Status code: {response.status_code}")
 
+        # 3. Проверка HTTP-статуса. Генерирует исключение для кодов 4xx и 5xx.
+        response.raise_for_status()
+
+        logger.info(f"Ответ но названию города получен. Status code: {response.status_code}")
+
+        # 4. Десериализация JSON в словарь.
         return response.json()
 
+    except requests.exceptions.HTTPError as http_err:
+        logger.error(f"HTTP ошибка: {http_err}")
+    except requests.exceptions.ConnectionError as errc:
+        logger.error(f"Ошибка соединения: {errc}")
     except Exception as e:
         logger.exception(f"Ошибка запроса города: {e}")
-        return None
+
+    return None
